ae130e02f2cbf4155fca26a4e63101fd8424923a
Sean Abbas <sean.abbas33@gmail.com>
2023-03-22 11:56:51 -0400

    light switch

--- practice-for-week-14-react-context-greenhouse-long-practice-main/practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,28 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
+import { useContext } from 'react';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
 
+  const { themeName, setThemeName } = useTheme();
+
+  function showTheme() {
+    if (themeName === 'day') {
+      return dayImage;
+    } else {
+      return nightImage;
+    }
+  }
+
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={showTheme()}
             alt='greenhouse' 
       />
       <LightSwitch />
--- practice-for-week-14-react-context-greenhouse-long-practice-main/practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,22 @@
 import './LightSwitch.css';
+import { useContext } from 'react';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+
+  function changeTheme(time) {
+    if (time === 'day') {
+      setThemeName('day');
+    } else {
+      setThemeName('night');
+    }
+  }
+
   return (
     <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+      <div className="on" onClick={() => changeTheme('day')}>DAY</div>
+      <div className="off" onClick={() => changeTheme('night')}>NIGHT</div>
     </div>
   );
 }
